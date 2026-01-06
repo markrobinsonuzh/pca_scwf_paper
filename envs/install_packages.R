44d5a5e1d5074f17cfe1eb4c82413d667a414a6d
Mark Robinson <mark.robinson@mls.uzh.ch>
2026-01-06 08:53:15 +0100

    few more mods to R pkg install

--- envs/install_packages.R
+++ envs/install_packages.R
@@ -1,4 +1,10 @@
-install.packages("BiocManager")
+options(repos = "https://stat.ethz.ch/CRAN",
+	BioC_mirror="https://ftp.gwdg.de/pub/misc/bioconductor")
+
+install.packages("BiocManager", quiet=TRUE)
+BiocManager::install(version = "3.20")
+BiocManager::install("remotes", quiet=TRUE, ask=FALSE)
+install.packages("https://cran.r-project.org/src/contrib/Archive/TFMPvalue/TFMPvalue_0.0.9.tar.gz", repos=NULL, quiet=TRUE, ask=FALSE)
 
 pkgs <- 
 c("stringi", 
@@ -26,18 +32,18 @@ c("stringi",
 "dplyr",
 "Seurat",
 "patchwork",
-"BPCells",
+#"BPCells",
 "SeuratObject",
-"SeuratDisk",
+#"SeuratDisk",
 "scrapper",
 "parallel",
 "TENxBrainData",
 "EnsDb.Hsapiens.v75",
-"satijalab/azimuth",
-"bnprks/BPCells/r",
 "biomaRt",
 "DropletUtils")
 
-BiocManager::install(pkgs, Ncpus=10, quite=TRUE)
-    
+BiocManager::install(pkgs, Ncpus=20, ask=FALSE)
 
+BiocManager::install("satijalab/azimuth", ask=FALSE)
+BiocManager::install("mojaveazure/seurat-disk", ask=FALSE)
+BiocManager::install("bnprks/BPCells/r", ask=FALSE)
